2c7b4e8f08db8573d349290db309974d750a2cda
SamratSingh19 <samratsingh19nov@gmail.com>
2026-02-26 12:43:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+const fs=require("fs");
+
+
+app.set("view engine","ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res)=>{
+    // res.send("Hello Bachoon!");
+    fs.readdir("./files",function(err,files)
+    {
+        res.render("index",{files:files});
+    });
+});
+
+app.get("/file/:filename",(req,res)=>{
+    fs.readFile(`./files/${req.params.filename}`,"utf-8",(err,filedata)=>{
+        res.render("show",{filename:req.params.filename,filedata:filedata});
+    });
+
+});
+
+app.post("/delete/:filename",(req,res)=>{
+    fs.unlink("./files/"+req.params.filename,(err)=>{
+        if(err)console.log("Unable to delete file");
+    });
+    res.redirect("/");
+});
+app.post("/create",(req,res)=>{
+    fs.writeFile(`./files/${req.body.Title}.txt`,req.body.Details,function (err){});
+    res.redirect("/");
+})
+
+app.listen(3000);
